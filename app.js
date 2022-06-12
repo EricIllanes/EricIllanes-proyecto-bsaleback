9d5c947f9cb793fc3bb4700efb114dbd9cbaa22e
EricIllanes <e.icuriqueo@gmail.com>
2022-06-12 03:47:30 -0400

    update

--- app.js
+++ app.js
@@ -6,23 +6,22 @@ const routes = require("./routes");
 const cookieParser = require("cookie-parser");
 const { PORT, URL } = process.env;
 const morgan = require("morgan");
-const bodyParser = require("body-parser");
 
-// app.use(cors());
-// app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-// app.use(express.json({ limit: "50mb" }));
-// app.use(cookieParser());
-// app.use(morgan("dev"));
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", URL);
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "*"
-//   );
-//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-//   next();
-// });
+app.use(cors());
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", URL);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "*"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 // let corsOptions = {
 //   origin: "*",
 //   optionsSuccessStatus: 200,
@@ -45,7 +44,6 @@ const bodyParser = require("body-parser");
 //   next();
 // });
 
-app.use(cors({ origin: "*" }));
 
 app.use(routes);
 
