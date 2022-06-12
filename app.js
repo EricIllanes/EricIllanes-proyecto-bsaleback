66e6c480656f9eb61508d1a0890c8d1ff97e6965
EricIllanes <e.icuriqueo@gmail.com>
2022-06-12 01:09:52 -0400

    update

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const express = require("express");
 const app = express();
 const routes = require("./routes");
 const cookieParser = require("cookie-parser");
-const {PORT, URL} = process.env;
+const { PORT, URL } = process.env;
 const morgan = require("morgan");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 // app.use(cors());
 // app.use(express.urlencoded({ extended: true, limit: "50mb" }));
@@ -23,21 +23,25 @@ const bodyParser = require('body-parser');
 //   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
 //   next();
 // });
-app.use(cors());
+let corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', URL); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header("Access-Control-Allow-Origin", URL); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
 app.use(routes);
 
-app.listen(PORT || 80, () => {
+app.listen(PORT, () => {
   console.log("Server listening :)");
 });
