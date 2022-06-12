815878ab23ef80347b1650280825bd375790505d
EricIllanes <e.icuriqueo@gmail.com>
2022-06-12 03:50:37 -0400

    update

--- app.js
+++ app.js
@@ -22,28 +22,6 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
-// let corsOptions = {
-//   origin: "*",
-//   optionsSuccessStatus: 200,
-// };
-// console.log(222, URL);
-// app.use(cors(corsOptions));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(morgan("dev"));
-// app.use((req, res, next) => {
-
-//   res.header("Access-Control-Allow-Origin", URL); // update to match the domain you will make the request from
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-//   next();
-// });
-
 
 app.use(routes);
 
