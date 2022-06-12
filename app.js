446315507b033fb4eb6ee8e1bbefa9f2a989a4a6
EricIllanes <e.icuriqueo@gmail.com>
2022-06-12 04:00:07 -0400

    update

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", URL);
+  // res.header("Access-Control-Allow-Origin", URL);
+  res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
