5d3e7d6dca1c3e6620f0ca01f02a95d3d3f5d34a
EricIllanes <e.icuriqueo@gmail.com>
2022-06-12 00:46:57 -0400

    update

--- app.js
+++ app.js
@@ -6,23 +6,35 @@ const routes = require("./routes");
 const cookieParser = require("cookie-parser");
 const {PORT, URL} = process.env;
 const morgan = require("morgan");
+const bodyParser = require('body-parser');
 
+// app.use(cors());
+// app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+// app.use(express.json({ limit: "50mb" }));
+// app.use(cookieParser());
+// app.use(morgan("dev"));
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", URL);
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "*"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 app.use(cors());
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-app.use(express.json({ limit: "50mb" }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", URL);
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "*"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
-
 
 app.use(routes);
 
