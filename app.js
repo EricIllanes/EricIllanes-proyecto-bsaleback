6039508078a64486015decb84c7d898dc189c0d5
EricIllanes <e.icuriqueo@gmail.com>
2022-06-12 00:19:52 -0400

    up

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const routes = require("./routes");
 const cookieParser = require("cookie-parser");
-const {PORT} = process.env;
+const {PORT, URL} = process.env;
 const morgan = require("morgan");
 
 app.use(cors());
@@ -13,7 +13,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", URL);
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
